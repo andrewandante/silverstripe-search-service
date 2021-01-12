06957256912640d3f4e4f18ccfaa2c5585d9dc42
Andrew Aitken-Fincham <andrew.aitkenfincham@silverstripe.com>
2021-01-12 15:04:04 +1300

    styling, docs counts

--- src/Admin/SearchAdmin.php
+++ src/Admin/SearchAdmin.php
@@ -15,7 +15,9 @@ use SilverStripe\ORM\DataObject;
 use SilverStripe\ORM\FieldType\DBInt;
 use SilverStripe\ORM\FieldType\DBText;
 use SilverStripe\SearchService\Extensions\SearchServiceExtension;
+use SilverStripe\SearchService\Interfaces\DocumentFetcherInterface;
 use SilverStripe\SearchService\Interfaces\IndexingInterface;
+use SilverStripe\SearchService\Services\AppSearch\AppSearchService;
 use SilverStripe\View\ArrayData;
 use Symfony\Component\VarDumper\Cloner\Data;
 
@@ -39,25 +41,28 @@ class SearchAdmin extends LeftAndMain
         $docsURL = $indexService->getDocumentationURL();
 
         if ($externalURL !== null || $docsURL !== null) {
-            $fields[] = HeaderField::create('ExternalLinksHeader', 'External Links');
-        }
+            $fields[] = HeaderField::create('ExternalLinksHeader', 'External Links')
+                ->setAttribute('style', 'font-weight: 300;');
 
-        if ($externalURL !== null) {
-            $fields[] = LiteralField::create(
-                'ExternalURL',
-                sprintf(
-                    '<div><a href="%s" target="_blank">%s</a></div>',
-                    $externalURL,
-                    $indexService->getExternalURLDescription() ?? 'External URL'
-                ),
-            );
-        }
+            if ($externalURL !== null) {
+                $fields[] = LiteralField::create(
+                    'ExternalURL',
+                    sprintf(
+                        '<div><a href="%s" target="_blank" style="font-size: large">%s</a></div>',
+                        $externalURL,
+                        $indexService->getExternalURLDescription() ?? 'External URL'
+                    ),
+                );
+            }
 
-        if ($docsURL !== null) {
-            $fields[] = LiteralField::create(
-                'DocsURL',
-                sprintf('<div><a href="%s" target="_blank">Documentation URL</a></div>', $docsURL),
-            );
+            if ($docsURL !== null) {
+                $fields[] = LiteralField::create(
+                    'DocsURL',
+                    sprintf('<div><a href="%s" target="_blank" style="font-size: large">Documentation URL</a></div>', $docsURL),
+                );
+            }
+
+            $fields[] = LiteralField::create('Divider', '<div class="clear" style="height: 32px; border-bottom: 1px solid #ced5e1"></div>');
         }
 
         $indexedDocsGridfield = GridField::create(
@@ -74,12 +79,23 @@ class SearchAdmin extends LeftAndMain
     private function buildIndexedDocumentsList()
     {
         $list = ArrayList::create();
-        foreach (SearchServiceExtension::singleton()->getConfiguration()->getIndexes() as $index => $data) {
-            $dataObject = new IndexedDocumentsResult();
-            $dataObject->IndexName = $index;
-            $dataObject->DBDocs = 123;
-            $dataObject->RemoteDocs = 456;
-            $list->push($dataObject);
+
+        /** @var IndexingInterface $indexer */
+        $indexer = Injector::inst()->get(IndexingInterface::class);
+
+        $configuration = SearchServiceExtension::singleton()->getConfiguration();
+        foreach ($configuration->getIndexes() as $index => $data) {
+
+            $localCount = 0;
+            foreach ($configuration->getClassesForIndex($index) as $class) {
+                $localCount += $class::get()->where('SearchIndexed IS NOT NULL')->count();
+            }
+
+            $result = new IndexedDocumentsResult();
+            $result->IndexName = AppSearchService::environmentizeIndex($index);
+            $result->DBDocs = $localCount;
+            $result->RemoteDocs = $indexer->getDocumentTotal($index);
+            $list->push($result);
         }
 
         return $list;
